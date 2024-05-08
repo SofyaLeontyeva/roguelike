5cdacbe660a2ba3a36eb962a676eb60e756f550d
Starvaruch <113064906+Starvaruch@users.noreply.github.com>
2024-05-08 17:49:14 +0300

    Update ObjKey.java

--- src/main/java/object/ObjKey.java
+++ src/main/java/object/ObjKey.java
@@ -1,5 +1,26 @@
 package object;
 
+import java.io.IOException;
+import java.util.Objects;
+import javax.imageio.ImageIO;
+import logic.GamePanel;
+
 public class ObjKey extends SuperObject {
 
+    GamePanel gamePanel;
+
+    public ObjKey(GamePanel gamePanel) {
+
+        setName("Key");
+        try {
+            setImage(getScaleTool().scaleImage(
+                    ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/objects/key.png"))),
+                    gamePanel.getTileSize(),
+                    gamePanel.getTileSize()
+            ));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
